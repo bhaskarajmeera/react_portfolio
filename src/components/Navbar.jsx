22f56d2e43006f22347c1dad0ba277170ae2feb0
bhaskarajmeera <bhaskar.ajmeera@zohomail.com.au>
2026-05-12 14:33:29 +1000

    Navbar migrtaed from dom based to react based

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,33 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <div>
-         
-    </div>
-  )
-}
+    <>
+    <header>
+        <div class="name container flex ">
+        <div class="logo flex">
+            <div>Bhaskar Ajmeera</div>
+            <div class="line">Soft.Engineer</div>
+        </div>
+        <div>
+        <label for="hamburgerMenu">
+          <i class="fa-solid fa-bars"></i>
+        </label>
+        <input type="checkbox" id="hamburgerMenu"/>
+        </div>
+        <div class="menu">
+            <ul class="flex navigation">
+            <li><a href="#hero">
+                Home</a></li>
+            <li><a href="#skills"
+                >Skills</a></li>
+            <li><a href="#projects
+                ">Projects</a></li>
+            <li><a href="#about">About me</a></li>
+            <li><a href="#contact">Conact us</a></li>
+            </ul>
+        </div>
+        </div>
+      </header>  
+    </>
+  );
+};
