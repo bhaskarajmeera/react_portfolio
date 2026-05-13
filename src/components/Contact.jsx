324cd2a7e77059c571d55d165b65237670e20ac6
bhaskarajmeera <bhaskar.ajmeera@zohomail.com.au>
2026-05-13 16:54:41 +1000

    added about me and contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,31 @@ import { Title } from "./Title";
 export const Contact = () => {
   return (
     <section className="contact" id="contact">
-      
+      <Title title="Contact" />
+      <div className="flex social">
+        <a href="www.linkedin.com/in/bhaskarajmeera">
+          <i className="fa-brands fa-linkedin" style={{ color: "blue" }}></i>
+        </a>
+        <a href="https://github.com/bhaskarajmeera">
+          <i className="fa-brands fa-github" style={{ color: "black" }}></i>
+        </a>
+        <a href="https://www.youtube.com/">
+          <i className="fa-brands fa-youtube" style={{ color: "red" }}></i>
+        </a>
+        <a href="">
+          <i className="fa-solid fa-mobile" style={{ color: "grey" }}></i>
+        </a>
+      </div>
+      <h3>OR</h3>
+
+      <div className="flex-center">
+        <a href="" className="email-section flex-center">
+          <span>youremail@gmail.com</span>
+          <div className="email-icon flex-center">
+            <i className="fa-solid fa-paper-plane" style={{ color: "blue" }}></i>
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
